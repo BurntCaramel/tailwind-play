be41b4459841c2fc0bf6e95639c171163a6ccfd8
Patrick Smith <patrick@burntcaramel.com>
2017-11-13 15:25:26 +1100

    Add CSS processing

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,22 @@
 const Hapi = require('hapi')
+const Path = require('path')
+const postcss = require('postcss')
+const tailwind = require('tailwindcss')
+
+const defaultTailwindConfigPath = Path.join(__dirname, 'tailwind.js')
+//const defaultConfig = require('tailwindcss').defaultConfig()
+//const defaultTailwindConfig = require('tailwindcss/defaultConfig')
+
+const defaultCSS = `
+@tailwind preflight;
+@tailwind utilities;
+`
+
+function processCSS(css) {
+  return postcss([tailwind(defaultTailwindConfigPath)])
+  .process(css)
+  .then(result => result.css)
+}
 
 const server = new Hapi.Server()
 
@@ -10,6 +28,26 @@ server.route([
     path: '/',
     handler(request, reply) {
       reply({ success: true })
+    }
+  },
+  {
+    method: 'POST',
+    path: '/process',
+    handler(request, reply) {
+      reply(
+        processCSS(request.payload)
+      )
+      .type('text/css')
+    }
+  },
+  {
+    method: 'GET',
+    path: '/default.css',
+    handler(request, reply) {
+      reply(
+        processCSS(defaultCSS)
+      )
+      .type('text/css')
     }
   }
 ])
